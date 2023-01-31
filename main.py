5a7d3eff2e90b9bfc2f5534b6348cde9bcd9e3f0
Ashot <110162000+shobviznali@users.noreply.github.com>
2023-01-31 15:54:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ def sorting_lists(list1: list, list2: list):
     print(sorted(list1 + list2))
     print(sorted(list1 + list2, reverse=True))
 
+# 7
+
+def is_polindrom(str1: str) -> bool:
+    reversed = str1[::-1]
+    if str1 == reversed:
+        return True
+    return False
